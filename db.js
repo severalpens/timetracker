26849a69e51ab81ea6fbb8aa41b7299fb9611166
Severalpens <Paul.Collins@ibm.com>
2022-04-29 19:18:43 +1000

    add db.js

---
+++ db.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+const host=process.env.host;
+const database=process.env.database;
+const username=process.env.user;
+const password=process.env.password;
+
+const options = {
+    dialect: 'mssql',
+    host
+}
+
+const sequelize = new Sequelize(database, username, password,options);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
